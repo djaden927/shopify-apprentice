16da576bfd24229405685632d9f4e3734b91a76b
djaden927 <djaden92@gmail.com>
2021-09-21 21:44:22 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-
+import ImageCard from "./components/imageCards.js"
 
 function App() {
   const [images, setImages] = useState([])
 
+  const apikey = process.env.REACT_APP_API_KEY;
+
   useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5')
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apikey}&count=10`)
       .then((res) => {
-        setImages(res)
+        setImages(res.data)
       })
       .catch((err) => {
         console.log(err)
       })
-  },[])
+  }, [apikey])
+
   console.log(images)
   return (
     <div className="App">
-
+      <div class="stars"></div>
+      <div class="twinkling"></div>
+      <div class="clouds">
+      {images.length > 0 && <ImageCard images={images}/>}
+      </div>
+      
 
     </div>
   );
---
+++ src/components/imageCards.js
@@ -0,0 +1,111 @@
+
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
+
+const DateHolder = styled.div
+  `
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  z-index: 7;
+  height: 90vh;
+`
+
+const DateBox = styled.div
+  `
+  display: flex;
+  justify-content: center;
+  width: 70%;
+  flex-wrap: wrap;
+  z-index: 6;
+  background-color: white;
+  margin: 1.5%;
+`
+
+const Photo = styled.img
+  `
+  width: 75%;
+  height: auto;
+  margin-top: 1%;
+`
+
+const Explanation = styled.p
+  `
+  margin: 1%;
+  border: 2px solid #000000;
+  background-color: #979797;
+  padding: .5%;
+  line-height: 1.5;
+`
+
+const Like = styled.div
+`
+  margin .5% 20%;
+  width: 20%;
+`
+
+const Next = styled.button
+  `
+height: 4em;
+width: 20%;
+padding: 1.5em auto;
+margin: 1em auto;
+background-color: #32CD30;
+border: none;
+border-radius: 3px;
+text-transform: uppercase;
+letter-spacing: 0.5em;
+transition: all 0.2s cubic-bezier(.4,0,.2,1);
+color: white;
+
+&:hover{
+  letter-spacing: 0.8em;
+  background-color: #76B947;
+}
+`
+
+function ImageCard(props) {
+  const { images } = props
+  const [active, setActive] = useState("hidden")
+  const [count, setCount] = useState(0)
+  
+
+  let likeActive = ""
+  
+
+  const likeClick = (evt) => {
+    if(active === "hidden"){
+      setActive('')
+      likeActive = "like-active";
+    }else {
+      setActive('hidden')
+      likeActive = "";
+    }
+  }
+
+  const nextPhoto = () => {
+    setActive('hidden')
+    likeActive = "";
+    setCount(count + 1)
+    console.log(count)
+  }
+
+  return (
+    <DateHolder className='hero'>
+
+          <DateBox>
+            <Photo src={images[count].hdurl} alt={images[count].title} />
+            <Like onClick={likeClick} className='like'>
+              <button className={`${likeActive} like-toggle basic2`} > ♥</button>
+              <span className={active}>I like this</span>
+            </Like>
+            <h1>{images[count].title} taken on {images[count].date}</h1>
+            <Explanation>{images[count].explanation}</Explanation>
+            <Next className="nextPhoto" onClick={nextPhoto}>Next Photo</Next>
+          </DateBox>
+          
+    </DateHolder>
+  );
+}
+
+export default ImageCard;
